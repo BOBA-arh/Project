3877f21b68c9261d1454953fe6c9b19d057eb57e
BOBA <makeewova88@gmail.com>
2021-12-02 18:06:20 +0300

    333

--- index.php
+++ index.php
@@ -4,9 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title><?= "New"; ?></title>
 </head>
 <body>
+    <?php
+    echo date(DATE_RSS);
+    ?>
     Hello world!
     <h1>The World!</h1>
 </body>
